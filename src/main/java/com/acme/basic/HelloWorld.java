eb50581b80852b0defda52716884d36056a056a6
sowmianreddy <sowmianreddy@gmail.com>
2020-03-28 16:26:51 -0700

    updated

--- src/main/java/com/acme/basic/HelloWorld.java
+++ src/main/java/com/acme/basic/HelloWorld.java
@@ -3,7 +3,7 @@ package com.acme.basic;
 public class HelloWorld {
 
   void sayHello() {
-    System.out.println("Hello World from pipeline again ...change and test");
+    System.out.println("Hello World from pipeline again ...change and test now");
   }
 
   void notCovered() {
